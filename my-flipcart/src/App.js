53e88ae72f6797ccefc5e0375a4c29b5679a12d0
krujjwal <ujjwal-kr@outlook.com>
2021-06-08 11:50:17 +0530

    project flipkart

---
+++ my-flipcart/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Flipcart from './Component/LandingPage/Flipcart'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Flipcart />
+      </div>
+    )
+  }
+}
